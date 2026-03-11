f7c7d6855adc228d3daa86c3967b3a960d77aaee
BobaSpacetime <163637926+BobaSpacetime@users.noreply.github.com>
2026-03-11 19:56:32 +0100

    Add files via upload

--- gamez.py
+++ gamez.py
@@ -10,12 +10,21 @@ f = Actor("flower")
 f.x = random.randint(50,400)
 f.y = random.randint(50, 400)
 score = 0
+gameover = False
 def draw():
     screen.blit("background", (0,0))
     b.draw()
     f.draw()
+    screen.draw.text("Score: " + str(score), (10,10), color = "#073A11")
+    if gameover==True:
+        screen.fill("#1E0A3A")
+        screen.draw.text("TIME'S UP! Your score was: " + str(score) + " in 1 minute", center = (225, 225), color = "#B79CEB")
+    if score>=30:
+        screen.fill("#ECD030")
+        screen.draw.text("YOU WIN!", center = (225, 225), color = "#FFFC39")
 
 def update():
+    global score
     if keyboard.left: 
         b.x-=1
     if keyboard.right:
@@ -25,7 +34,13 @@ def update():
     if keyboard.down:
         b.y+=1
     if b.colliderect(f):
+        score +=1
         f.x = random.randint(50, 400)
         f.y = random.randint(50, 400)
 
+def timesup():
+    global gameover
+    gameover = True 
+
+clock.schedule(timesup, 60.0)
 pgzrun.go()
